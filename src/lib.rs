73eb8f7fe8e51340cb3be7c133494bfe29e2f002
Sebastian <sebjfk@gmail.com>
2019-06-09 02:26:49 -0400

    change plugin class name to Laminar

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,33 @@
 #[macro_use]
 extern crate gdnative as godot;
+extern crate crossbeam_channel;
+extern crate serde_derive;
+extern crate laminar;
+use crossbeam_channel::{Receiver, Sender};
+use laminar::{ErrorKind, Packet, Socket, SocketEvent};
+use serde_derive::{Deserialize, Serialize};
+use std::net::SocketAddr;
+use std::{thread, time};
 
 #[derive(gdnative::NativeClass)]
 #[inherit(gdnative::Node)]
-struct Client {}
+struct Laminar {
+    client: Option<Client>,
+}
+
+struct Client {
+    packet_sender: Sender<Packet>,
+    _event_receiver: Receiver<SocketEvent>,
+    _polling_thread: thread::JoinHandle<Result<(), ErrorKind>>,
+}
 
 #[gdnative::methods]
-impl Client {
+impl Laminar {
 
     fn _init(_owner: gdnative::Node) -> Self {
-        Client {}
+        Laminar {
+            client: None
+        }
     }
 
     #[export]
@@ -21,10 +39,17 @@ impl Client {
     fn send(&self, _owner: gdnative::Node, message: godot::GodotString) {
         godot_print!("send packet: {}", message.to_string())
     }
+
+    #[export]
+    fn new(&self, _owner: gdnative::Node, address: godot::GodotString) {
+        // setup an udp socket and bind it to the client address.
+        let (mut socket, packet_sender, event_receiver) = Socket::bind(address.to_string()).unwrap();
+        let polling_thread = thread::spawn(move || socket.start_polling());
+    }
 }
 
 fn init(handle: gdnative::init::InitHandle) {
-    handle.add_class::<Client>();
+    handle.add_class::<Laminar>();
 }
 
 godot_gdnative_init!();
